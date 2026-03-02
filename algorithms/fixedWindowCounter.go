74b82d1c01e7e344061e0a866ade1a10974ba614
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2026-03-02 13:31:16 +0530

    Update fixedWindowCounter.go

--- algorithms/fixedWindowCounter.go
+++ algorithms/fixedWindowCounter.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"fmt"
 	"goapp/constants"
+	"goapp/lua"
 	"goapp/store"
 	"time"
 
-	"github.com/bytedance/sonic"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -29,51 +29,18 @@ func GetNewFixedWindowCounter(window time.Duration, capacity int64) *FixedCounte
 }
 
 func (fc *FixedCounter) Allow(ctx context.Context, tenandId, userId string) (bool, error) {
-	tokens := &FixedCounterStore{}
-	// reset if the window is crossed
-	now := time.Now()
+	now := time.Now().UnixNano()
 
-	currentWindowIndex := now.UnixNano() / int64(fc.window)
-
-	// read data from the redis
 	redisKey := fmt.Sprintf("%s:%s:%s:%s", constants.KeyRateLimit, constants.AlgorithmFixedWindow, tenandId, userId)
 
-	val, err := store.Rdb.Get(ctx, redisKey).Result()
-	if err == redis.Nil {
-		tokens.WindowIndex = currentWindowIndex
-		tokens.Allowed = fc.capacity
-	} else if err != nil {
-		fmt.Println("Error reading data from redis : ", err)
+	fwcScript := redis.NewScript(lua.GetFixedWindowCounterScript())
+
+	_, err := fwcScript.Run(ctx, store.Rdb, []string{redisKey}, fc.capacity, fc.window, now, 1).Result()
+	if err != nil {
+		fmt.Println("Error calling the fixed window counter script, rejecting the request : ", err)
 		return false, err
 	} else {
-		// unmarshal the data
-		err := sonic.Unmarshal([]byte(val), tokens)
-		if err != nil {
-			fmt.Println("Error unmarshalling data from redis : ", err)
-			return false, err
-		}
+		fmt.Println("Accepting the request")
 	}
-
-	if currentWindowIndex > tokens.WindowIndex {
-		tokens.WindowIndex = currentWindowIndex
-		tokens.Allowed = fc.capacity
-	}
-
-	if tokens.Allowed == 0 {
-		fmt.Println("Request is denied")
-		return false, nil
-	}
-
-	tokens.Allowed -= 1
-	fmt.Println("Request is allowd")
-
-	// Store the data into redis
-	marshedVal, err := sonic.Marshal(tokens)
-	if err != nil {
-		fmt.Println("Error marshalling data to redis : ", err)
-		return false, err
-	}
-
-	store.Rdb.Set(ctx, redisKey, marshedVal, fc.window)
 	return true, nil
 }

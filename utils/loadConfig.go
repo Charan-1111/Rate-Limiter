836b0b7c0967245139085b0ec092bcb1091e60cb
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2026-03-10 10:20:00 +0530

    Update loadConfig.go

--- utils/loadConfig.go
+++ utils/loadConfig.go
@@ -2,16 +2,19 @@ package utils
 
 import (
 	"goapp/models"
+	"goapp/store"
 	"os"
 
 	"github.com/bytedance/sonic"
 )
 
 type Config struct {
-	Ports      models.Ports       `json:"ports"`
-	Redis      models.RedisConfig `json:"redis"`
-	MaxTokens  float64            `json:"maxTokens"`
-	RefillRate float64            `json:"refillRate"`
+	Ports      models.Ports      `json:"ports"`
+	Database   store.Database    `json:"database"`
+	Redis      store.RedisConfig `json:"redis"`
+	Tables     map[string]string `json:"tables"`
+	MaxTokens  float64           `json:"maxTokens"`
+	RefillRate float64           `json:"refillRate"`
 }
 
 func (config *Config) LoadConfig(filePath string) error {

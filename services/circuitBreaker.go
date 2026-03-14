5f96ca63c4e3ec2812913bd40babee0f8811383d
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2026-03-14 10:50:49 +0530

    Update circuitBreaker.go

--- services/circuitBreaker.go
+++ services/circuitBreaker.go
@@ -1,3 +1,28 @@
-package services 
+package services
 
-type Circu
+import (
+	"goapp/constants"
+
+	"github.com/sony/gobreaker"
+)
+
+type CircuitBreaker struct {
+	cb *gobreaker.CircuitBreaker
+}
+
+func NewCircuitBreaker() *CircuitBreaker {
+	settings := gobreaker.Settings{
+		Name:        "CircuitBreaker",
+		MaxRequests: 2,                                // half-open state allowed requests
+		Interval:    constants.CircuitBreakerInterval, // time to reset counts
+		Timeout:     constants.CircuitBreakerTimeout,  // time to stay in open state
+
+		ReadyToTrip: func(counts gobreaker.Counts) bool {
+			return counts.ConsecutiveFailures >= constants.ConsecutiveFailuresThreshold
+		},
+	}
+
+	return &CircuitBreaker{
+		cb: gobreaker.NewCircuitBreaker(settings),
+	}
+}

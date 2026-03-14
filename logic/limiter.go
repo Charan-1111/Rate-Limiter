f004c6b349828663f486c48a1a166a481511ed29
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2026-03-14 16:35:45 +0530

    Update limiter.go

--- logic/limiter.go
+++ logic/limiter.go
@@ -11,11 +11,11 @@ import (
 	"github.com/rs/zerolog"
 )
 
-func GetLimiter(ctx context.Context, db *pgxpool.Pool, rdb *redis.Client, config *utils.Config, log zerolog.Logger, limiterFactory algorithms.LimiterFactory, cache *services.Cache, scope, identifier, rateLimitType string) {
+func GetLimiter(ctx context.Context, db *pgxpool.Pool, rdb *redis.Client, config *utils.Config, log zerolog.Logger, limiterFactory algorithms.LimiterFactory, cache *services.Cache, cb *services.CircuitBreaker, scope, identifier, rateLimitType string) {
 	limiter, err := limiterFactory.GetLimiter(ctx, db, log, scope, identifier, rateLimitType, config.Queries.Fetch.FetchPolicyByKey, cache)
 	if err != nil {
 		log.Error().Err(err).Msg("Error getting the limiter interface")
 	}
 
-	limiter.Allow(ctx, rdb, scope, identifier)
+	limiter.Allow(ctx, rdb, cb, log, scope, identifier)
 }

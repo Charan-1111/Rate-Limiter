9c04ca740ca83e92e289ead26d6d28ace2f1e2d7
Charan <ravivijayacharan0@gmail.com>
2026-04-06 22:50:07 +0530

    initiated structs and interfaces

--- store/storeRepository.go
+++ store/storeRepository.go
@@ -1,5 +1,20 @@
 package store
 
+import (
+	"goapp/services"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog"
+)
+
 type LimiterRepository interface {
-	
+	FetchPolicies() map[string]*services.PolicySchema
+	FetchPolicyByKey(key string) (*services.PolicySchema, bool)
+}
+
+type StoreStruct struct {
+	db  *pgxpool.Pool
+	rdb *redis.Client
+	log zerolog.Logger
 }
